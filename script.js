9d2a53cf6ee95831c463191bc5f224df17ea4be2
simbu25 <asilambu53@gmail.com>
2023-10-14 11:53:56 +0530

    resume file

---
+++ script.js
@@ -0,0 +1,26 @@
+let menuBtn = document.getElementById("menuBtn")
+let sideNav = document.getElementById("sideNav")
+
+sideNav.style.right = "-250px";
+menuBtn.onclick = function(){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+    }
+    else{
+        sideNav.style.right = "-250px";
+    }
+}
+
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+    speed: 500,
+    speedAsDuration: true
+});
+
+function getValue(){
+    var val=prompt("Just Enter Your Name! :)");
+}
+
+function succes(){
+    alert("Your Registration Successful!");
+}
